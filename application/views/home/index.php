f5650ef045fb04ff9b1228aad7e1344cb5e0dfb4
WahyuS002 <syahputrawahyu61@gmail.com>
2019-11-10 14:11:19 +0700

    Membuat tampilan front end dengan MVC

---
+++ application/views/home/index.php
@@ -0,0 +1,104 @@
+<header class="container-fluid grey1 navbar">
+    <h4> <i class="fas fa-paw"></i> SIMBA </h4>
+    <h4 class="btn btn-2">UPLOAD LOMBA</h4>
+</header>
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4"> SISTEM INFORMASI LOMBA </h1>
+        <p class="lead">University of Bengkulu</p>
+    </div>
+</div>
+
+<ul class="nav justify-content-center text-center">
+    <li class="nav-item">
+        <a class="nav-link active" href="#"> <i class="fas fa-book"></i><br> ALL </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active" href="#"> <i class="fas fa-paint-brush"></i><br> DESIGN </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#"> <i class="fas fa-code"></i> <br> PROGRAMMING </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#"> <i class="fas fa-gamepad"></i> <br> GAMING </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#"> <i class="fas fa-futbol"></i> <br> SPORT </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#"> <i class="fas fa-pencil-ruler"></i> <br> ACADEMIC </a>
+    </li>
+
+</ul>
+
+<div class="container-fluid ">
+    <div class="row content">
+        <div class="col-lg-3 px-5 sidebar">
+            <ul class="nav flex-column text-center">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Menu-1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Menu-2</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Menu-3</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Menu-4</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col-lg-9 col-md-12 col-sm-12 text-center">
+            <div class="card" style="width: 18rem;">
+                <img src="<?= base_url() ?>/assets/img/ex2.jpg" class="card-img-top" alt="gambar2">
+                <div class="card-body">
+                    <h5 class="card-title">Judul Lomba</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias temporibus aspernatur distinctio consequuntur veritatis quibusdam quidem id reprehenderit expedita voluptate.</p>
+                    <a href="lomba.html" class="btn btn-1">Detail lomba</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="<?= base_url() ?>/assets/img/ex2.jpg" class="card-img-top" alt="gambar2">
+                <div class="card-body">
+                    <h5 class="card-title">Judul Lomba</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias temporibus aspernatur distinctio consequuntur veritatis quibusdam quidem id reprehenderit expedita voluptate.</p>
+                    <a href="#" class="btn btn-1">Detail lomba</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="<?= base_url() ?>/assets/img/ex2.jpg" class="card-img-top" alt="gambar2">
+                <div class="card-body">
+                    <h5 class="card-title">Judul Lomba</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias temporibus aspernatur distinctio consequuntur veritatis quibusdam quidem id reprehenderit expedita voluptate.</p>
+                    <a href="#" class="btn btn-1">Detail lomba</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="<?= base_url() ?>/assets/img/ex2.jpg" class="card-img-top" alt="gambar2">
+                <div class="card-body">
+                    <h5 class="card-title">Judul Lomba</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias temporibus aspernatur distinctio consequuntur veritatis quibusdam quidem id reprehenderit expedita voluptate.</p>
+                    <a href="#" class="btn btn-1">Detail lomba</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="<?= base_url() ?>/assets/img/ex2.jpg" class="card-img-top" alt="gambar2">
+                <div class="card-body">
+                    <h5 class="card-title">Judul Lomba</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias temporibus aspernatur distinctio consequuntur veritatis quibusdam quidem id reprehenderit expedita voluptate.</p>
+                    <a href="#" class="btn btn-1">Detail lomba</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="<?= base_url() ?>/assets/img/ex2.jpg" class="card-img-top" alt="gambar2">
+                <div class="card-body">
+                    <h5 class="card-title">Judul Lomba</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias temporibus aspernatur distinctio consequuntur veritatis quibusdam quidem id reprehenderit expedita voluptate.</p>
+                    <button href="#" class="btn btn-1">Detail lomba</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ application/views/templates/header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/css/bootstrap.css">
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/css/styles.css">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/0e86436e00.js" crossorigin="anonymous"></script>
+    <title>Hello, world!</title>
+</head>
+
+<body>
